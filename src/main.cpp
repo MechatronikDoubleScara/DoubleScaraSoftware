c9ee97ac5f56da904fa12e443117246b75e90bc5
GeorgUr <georg.uray@sbg.at>
2018-02-21 09:33:26 +0100

    Update
    Update Gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
 #include <Arduino.h>
+#include <Configuration.h>
 
 void setup() {
-    // put your setup code here, to run once:
-    Serial.begin(9600);
+
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
-    int count;
 
 }
