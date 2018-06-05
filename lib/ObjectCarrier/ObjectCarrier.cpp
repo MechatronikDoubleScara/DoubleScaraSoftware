15629ad51072b05575f4aa536cf7623e76345201
PGotzmann <philipp.gotzmann@hotmail.com>
2018-06-05 09:39:14 +0200

    Adden ObjectCarrier
    Adden class implementation for object carrier.
    Implementet functions:
    - resolve coordinates for position
    - implemented different standard object carriers
    - mark unreachable/ not allowed/ occupied positions
    - remember places parts
    - support different colours, and reservated positions
    - find next free or occupied position

---
+++ lib/ObjectCarrier/ObjectCarrier.cpp
@@ -0,0 +1,170 @@
+#include <ObjectCarrier.h>
+
+ObjectCarrier::ObjectCarrier(int position_oc){
+
+  int origin_x;
+  int origin_y;
+  int inc;
+
+  if (position_oc == 0) {
+    origin_x = -OBJECTCARRIER_OFFSET_X;
+    origin_y = OBJECTCARRIER_OFFSET_Y;
+    inc = OBJECTCARRIER_INCREMENT;
+  }
+  else if (position_oc == 1){
+    origin_x = +OBJECTCARRIER_OFFSET_X;
+    origin_y = -OBJECTCARRIER_OFFSET_Y;
+    inc = -OBJECTCARRIER_INCREMENT;
+  }
+  else{
+    return;
+  }
+
+  for (int i=0, y=origin_y; i < OBJECTCARRIER_NUM_ROWS; i++, y += inc){
+    for (int j=0, x=origin_x; j < OBJECTCARRIER_NUM_COLS; j++, x += inc){
+      Position[j][i].x = x;
+      Position[j][i].y = y;
+    }
+  }
+}
+
+int ObjectCarrier::setMode(int type = 0){
+  switch (type) {
+    case 0: // standard object carrier; all reachable positions allowed
+
+      // set every status to 0
+      for (int i=0; i < OBJECTCARRIER_NUM_ROWS; i++){
+        for (int j=0; j < OBJECTCARRIER_NUM_COLS; j++){
+          Position[j][i].status = 0;
+        }
+      }
+      // mark unreachable positions with -1
+      Position[0][3].status = -1;
+      Position[0][4].status = -1;
+      Position[0][5].status = -1;
+      Position[1][4].status = -1;
+      Position[1][5].status = -1;
+      Position[2][5].status = -1;
+
+      Position[11][3].status = -1;
+      Position[11][4].status = -1;
+      Position[11][5].status = -1;
+      Position[10][4].status = -1;
+      Position[10][5].status = -1;
+      Position[9][5].status = -1;
+      break;
+
+    case 1: // Object carrier for TicTacToe; 3x3 positions allowed
+      for (int i=0; i < OBJECTCARRIER_NUM_ROWS; i++){
+        for (int j=0; j < OBJECTCARRIER_NUM_COLS; j++){
+          if (j > 2 || i > 2)
+            Position[j][i].status = -1; // mark unreachable positions with -1
+          else
+             Position[j][i].status = 0; // all others with 0
+        }
+      }
+      break;
+
+    case 2: // Magazine Object carrier for two colours; 4x4 for each colour
+      for (int i=0; i < OBJECTCARRIER_NUM_ROWS; i++){
+        for (int j=0; j < OBJECTCARRIER_NUM_COLS; j++){
+          if (j > 1 && j <= 5 && i > 0 && i <= 4 )
+            Position[j][i].status = -2; // reservate positon for colour 2
+          else if (j > 5 && j <= 9 && i > 0 && i <= 4 )
+            Position[j][i].status = -3; // reservate positon for colour 3
+          else
+            Position[j][i].status = -1; // mark unreachable positions with -1
+        }
+      }
+      break;
+
+    default: //no valid mode
+      return -1;
+  }
+}
+
+int ObjectCarrier::setPosition(int x_pos,int y_pos,int type = 1){
+  //check for errors
+  if (x_pos < 0 || x_pos >= OBJECTCARRIER_NUM_COLS || y_pos < 0 || y_pos >= OBJECTCARRIER_NUM_ROWS)
+    return -1; // position does not exist
+  else if (Position[x_pos][y_pos].status == -1)
+    return -2; // position not allowed
+  else if (Position[x_pos][y_pos].status == type)
+    return -3; // position not free
+  else if (Position[x_pos][y_pos].status != (-type) && Position[x_pos][y_pos].status != 0)
+    return  -4; // wrong type for position
+
+  Position[x_pos][y_pos].status = type;
+  return 0;
+}
+
+int ObjectCarrier::resetPosition(int x_pos,int y_pos,int lockType = 0){
+  //check for errors
+  if (x_pos < 0 || x_pos >= OBJECTCARRIER_NUM_COLS || y_pos < 0 || y_pos >= OBJECTCARRIER_NUM_ROWS)
+    return -1; // position does not exsist
+  else if (Position[x_pos][y_pos].status == -1)
+    return -2; // position not allowed
+  else if (Position[x_pos][y_pos].status <= 0)
+    return -3; // position is free, nothing to remove
+
+
+  if (lockType == 1 && Position[x_pos][y_pos].status != 1){ //standard type 1 can not be locked
+    Position[x_pos][y_pos].status = -(Position[x_pos][y_pos].status);
+  }
+  else{
+    Position[x_pos][y_pos].status = 0;
+  }
+  return 0;
+}
+
+
+int ObjectCarrier::nextFreePosition(int& x_pos, int& y_pos, int type = 1){
+  if (type == 1) type = 0; // we are searching for empty positions without restrictions
+  else type = -(type); // we are searching for an empty positions for a specific colour
+
+  for (int i=0; i < OBJECTCARRIER_NUM_ROWS; i++){
+    for (int j=0; j < OBJECTCARRIER_NUM_COLS; j++){
+      if (Position[j][i].status == type){
+        x_pos = j;
+        y_pos = i;
+        return 0;
+      }
+    }
+  }
+  return -1;  //no suitable position left
+}
+
+int ObjectCarrier::nextOccupiedPosition(int& x_pos, int& y_pos, int type = 1){
+  for (int i=0; i < OBJECTCARRIER_NUM_ROWS; i++){
+    for (int j=0; j < OBJECTCARRIER_NUM_COLS; j++){
+      if (Position[j][i].status == type){
+        x_pos = j;
+        y_pos = i;
+        return 0;
+      }
+    }
+  }
+  return -1;  //no occupied position of type <type>
+}
+
+
+int ObjectCarrier::getState(int x_pos, int y_pos){
+  if (x_pos < 0 || x_pos >= OBJECTCARRIER_NUM_COLS || y_pos < 0 || y_pos >= OBJECTCARRIER_NUM_ROWS)
+    return -1; // position does not exist
+  else
+    return Position[x_pos][y_pos].status;
+}
+
+double ObjectCarrier::getCoordX(int x_pos, int y_pos){
+  if (x_pos < 0 || x_pos >= OBJECTCARRIER_NUM_COLS || y_pos < 0 || y_pos >= OBJECTCARRIER_NUM_ROWS)
+    return 0.0; // position does not exist
+  else
+    return Position[x_pos][y_pos].x;
+}
+
+double ObjectCarrier::getCoordY(int x_pos, int y_pos){
+  if (x_pos < 0 || x_pos >= OBJECTCARRIER_NUM_COLS || y_pos < 0 || y_pos >= OBJECTCARRIER_NUM_ROWS)
+    return 0.0; // position does not exist
+  else
+    return Position[x_pos][y_pos].y;
+}
--- lib/ObjectCarrier/ObjectCarrier.h
+++ lib/ObjectCarrier/ObjectCarrier.h
@@ -1,4 +1,54 @@
 #ifndef OBJECTCARRIER
 #define OBJECTCARRIER
 
+#define OBJECTCARRIER_OFFSET_X  110 // x-Offset from coordinat system origin to first element in object carrier (in mm)
+#define OBJECTCARRIER_OFFSET_Y  60  // y-Offset from coordinat system origin to first element in object carrier (in mm)
+#define OBJECTCARRIER_INCREMENT 20  // Increment between positions in the object carrier in both x and y (in mm)
+#define OBJECTCARRIER_NUM_ROWS  6   // Number of maximum rows in Object carrier (y-direction)
+#define OBJECTCARRIER_NUM_COLS  12  // Number of maximum colums in Object carrier (x-direction)
+
+// for initialisation of positon coordinates
+// use with constructor for parameter <position_oc>
+#define OBJECTCARRIER_UPPER     0   // relative positon of object carrier on +y side
+#define OBJECTCARRIER_LOWER     1   // relative positon of object carrier on -y side
+
+// for initalisation of available positions:
+// use with function setMode for parameter <mode>
+#define OBJECTCARRIER_MODE_STD  0   // standard object carrier; all reachable positions allowed
+#define OBJECTCARRIER_MODE_TICTACTOE  1 // Object carrier for TicTacToe; 3x3 positions allowed
+#define OBJECTCARRIER_MODE_MAGAZINE  2  // Magazine Object carrier for two colours; 4x4 for each colour
+
+
+class ObjPosition
+{
+  public:
+    ObjPosition() : status (0), x(0), y(0) {}
+    int status; // position free (0), occupied (1), occupied with colour (2,3...), reservated for colour (-2,-3,...), locked (-1)
+    int x;  // x coordinate (in mm)
+    int y;  // y coordinate (in mm)
+};
+
+class ObjectCarrier
+{
+  public:
+    ObjectCarrier(int position_oc);  // position_oc describes relative positon of object carrier (0 -> on +y side, 1-> on -y side)
+    int setMode(int mode = 0); // sets the mode for the object carrier and initialises positions states accordingly
+    int getMode() {return mode;}
+
+    int setPosition(int x_pos, int y_pos, int type = 1);
+    int resetPosition(int x_pos, int y_pos, int lockType = 0); // lockType = 0 just resets the positio (-> State = 0), lockType = 1 locks the position for the current colour (-> State =-<CURRENT STATE>)
+    int nextFreePosition(int& x_pos, int& y_pos,int type = 1);
+    int nextOccupiedPosition(int& x_pos, int& y_pos, int type = 1);
+
+    int getState(int x_pos, int y_pos);
+    double getCoordX(int x_pos, int y_pos);
+    double getCoordY(int x_pos, int y_pos);
+
+  private:
+    int mode;
+    ObjPosition Position[OBJECTCARRIER_NUM_COLS][OBJECTCARRIER_NUM_ROWS];
+};
+
+
+
 #endif
